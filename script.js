2abcc70709573d0c04851474aa5aa31cb60f5453
Saksham Awasthi <sakshamawasthi0912@gmail.com>
2024-09-19 18:09:26 +0530

    The clone of spotify website

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbum() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text()
 
     console.log(response)
@@ -111,12 +111,12 @@ async function displayAlbum() {
     for (let index = 0; index < array.length; index++) {
         const e = array[index];
 
-    if (e.href.includes("/songs/")) {
+    if (e.href.includes("/songs/" && !e.href.includes(".htaccess"))) {
         let folder = e.href.split("/").slice(-2)[0]
 
         //get the meta data of the folder
 
-        let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+        let a = await fetch(`/songs/${folder}/info.json`)
         let response = await a.json();
 
         cardContainer.innerHTML = cardContainer.innerHTML +
